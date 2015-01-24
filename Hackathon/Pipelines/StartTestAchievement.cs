d903ef1a5128dfe9a59ebefae30c09e54fd69e18
Dan Solovay <dan.solovay@velir.com>
2015-01-24 02:38:42 -0500

    Add StartTestAchievement class.
    Currently adds field to comment. Will be
    modified to track achievement later.

--- Hackathon/Pipelines/StartTestAchievement.cs
+++ Hackathon/Pipelines/StartTestAchievement.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using Sitecore.ContentTesting.Pipelines.StartTest;
+
 namespace Hackathon.Pipelines
 {
-    public class StartTestAchievement
+    public class StartTestAchievement : StartTestProcessor
     {
+        public override void Process(StartTestArgs args)
+        {
+            if (args.Test == null)
+                return;
+            var comment = Sitecore.Context.User.Profile.Comment;
+            comment += "Test started.";
+            Sitecore.Context.User.Profile.Comment = comment;
+            Sitecore.Context.User.Profile.Save();
+        }
     }
 }
