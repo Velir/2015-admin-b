17afb7a9ca0969332c979249fab0cae0fb0838f2
ccaplette <corey@velir.com>
2015-01-24 12:51:16 -0500

    1) Adding Util to test triggering of tests

---
+++ Hackathon/Models/Scratch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace Hackathon.Models
+{
+    public class Scratch : RenderingModel
+    {
+        public List<String> OutputOne { get; set; }
+        public string OutputTwo { get; set; }
+    }
+}
