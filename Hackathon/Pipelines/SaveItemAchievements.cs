ee325fb5b0467411a0b07611b6347c7db9228cc8
Dan Solovay <dan.solovay@velir.com>
2015-01-24 14:22:07 -0500

    Add logic to flag profile cards.

--- Hackathon/Pipelines/SaveItemAchievements.cs
+++ Hackathon/Pipelines/SaveItemAchievements.cs
@@ -11,6 +11,8 @@ namespace Hackathon.Pipelines
     {
         private const string _conditionTag = "<condition ";
 
+        private static ID TrackingFieldId = new ID("{B0A67B2A-8B07-4E0B-8809-69F751709806}");
+
         public void Process(SaveArgs args)
         {
             foreach (var item in args.Items)
@@ -18,20 +20,39 @@ namespace Hackathon.Pipelines
                 Item oldItem = Sitecore.Client.ContentDatabase.GetItem(item.ID);
                 if (oldItem == null) continue;
                  
-                string oldLayout = oldItem[FieldIDs.LayoutField];
-                string oldFinalRendering = oldItem[FieldIDs.FinalLayoutField];
-                 
-                 
-                SaveArgs.SaveField newLayout = item.Fields.FirstOrDefault(field => field.ID == FieldIDs.LayoutField);
-                SaveArgs.SaveField newFinal = item.Fields.FirstOrDefault(field => field.ID == FieldIDs.FinalLayoutField);
-                 
-                if (!oldLayout.Contains(_conditionTag) && !oldFinalRendering.Contains(_conditionTag) &&
-                    (HasConditionTag(newLayout) || HasConditionTag(newFinal)))
-                {
-                    Sitecore.Diagnostics.Log.Info("Personalizaton rule added", this);  //TODO Fire achievement instead.
-                }
+                RecordPersonalizationAchievement(oldItem, item);
+                RecordSetFirstProfileCard(oldItem, item);
             }
            
+        }
+
+        private void RecordSetFirstProfileCard(Item oldItem, SaveArgs.SaveItem item)
+        {
+            string oldTracking = oldItem[TrackingFieldId];
+
+            SaveArgs.SaveField newTracking = item.Fields.FirstOrDefault(field => field.ID == TrackingFieldId);
+
+
+            if (string.IsNullOrEmpty(oldTracking) && newTracking != null && !string.IsNullOrEmpty(newTracking.Value))
+            {
+                Sitecore.Diagnostics.Log.Info("Profile Card added.", this);
+            }
+
+        }
+
+        private void RecordPersonalizationAchievement(Item oldItem, SaveArgs.SaveItem item)
+        {
+            string oldLayout = oldItem[FieldIDs.LayoutField];
+            string oldFinalRendering = oldItem[FieldIDs.FinalLayoutField];
+
+            SaveArgs.SaveField newLayout = item.Fields.FirstOrDefault(field => field.ID == FieldIDs.LayoutField);
+            SaveArgs.SaveField newFinal = item.Fields.FirstOrDefault(field => field.ID == FieldIDs.FinalLayoutField);
+
+            if (!oldLayout.Contains(_conditionTag) && !oldFinalRendering.Contains(_conditionTag)
+                && (HasConditionTag(newLayout) || HasConditionTag(newFinal)))
+            {
+                Sitecore.Diagnostics.Log.Info("Personalizaton rule added", this); //TODO Fire achievement instead.
+            }
         }
 
         private static bool HasConditionTag(SaveArgs.SaveField savedField)
