723e65845dd0bbac2190405bc47cd15a1b5aa7e6
Dan Solovay <dan.solovay@velir.com>
2015-01-24 17:58:20 -0500

    Add solution and project files

---
+++ Hackathon/Pipelines/CreateBucketAchievement.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.Buckets.Pipelines.BucketOperations.CreateBucket;
+
+namespace AchievementTracker.Pipelines
+{
+    public class CreateBucketAchievement: CreateBucketProcessor
+    {
+        public override void Process(CreateBucketArgs args)
+        {
+            Sitecore.Diagnostics.Log.Info("Create Bucket Achievement unlocked!!", this);
+        }
+    }
+}
