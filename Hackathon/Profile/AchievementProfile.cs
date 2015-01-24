bd186936cadb7a4499c1d4cf547ecdea11d4451a
ccaplette <corey@velir.com>
2015-01-24 02:20:03 -0500

    Created AchievementProfile class with custom Achievement property
    Updated web.config to use custom profile class

---
+++ Hackathon/Profile/AchievementProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Profile
+{
+    public class AchievementProfile : Sitecore.Security.UserProfile
+    {
+        public string Achievement
+        {
+            get
+            {
+                return GetCustomProperty("achievement");
+            }
+            set
+            {
+                SetCustomProperty("achievement", value);
+                Save();
+            }
+        }
+    }
+}
