7b87dd7afbdc6d00ee4d7db2a174e16c92e1b6eb
AllTheAction <alex.jackson@velir.com>
2015-01-24 13:09:45 -0500

    Added control for displaying all users' achievements

--- Hackathon/sitecore/shell/client/YourApps/hackathon2015-admin-b/AchievementController.cs
+++ Hackathon/sitecore/shell/client/YourApps/hackathon2015-admin-b/AchievementController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Hackathon.Profile;
+using Sitecore.ApplicationCenter.Applications;
 using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Data.Managers;
@@ -16,7 +17,14 @@ namespace Hackathon.sitecore.shell.client.YourApps.hackathon2015_admin_b
 		{
 			Database db = Sitecore.Data.Database.GetDatabase("master");
 			var items = db.GetItems("{4233AB87-4CF3-46F2-9909-230510ECF4C6}").Where(i => i.TemplateID == new ID("{23AC8ACA-6407-4DF8-A548-53F62BB9ADDC}"));
-			return Json(items.Select(item => new Achievement { Name = item.GetField("Title"), Image = ThemeManager.GetImage(item.GetField("Icon"), 32,32) }), JsonRequestBehavior.AllowGet);
+
+			AchievementProfile userProfile = Sitecore.Context.User.Profile as AchievementProfile;
+			if (userProfile != null)
+			{
+				userProfile.Add("{9F8DF07B-FCEC-4607-BE3F-4576F4C3A4CC}");
+			}
+
+			return Json(items.Select(item => new Achievement { Name = item.GetField("Title"), Image = ThemeManager.GetImage(item.GetField("Icon"), 32, 32), Description = item.GetField("Description") }), JsonRequestBehavior.AllowGet);
 		}
 
 		public JsonResult GetMyAchievements()
@@ -24,6 +32,23 @@ namespace Hackathon.sitecore.shell.client.YourApps.hackathon2015_admin_b
 			AchievementManager achievementManager = new AchievementManager(Sitecore.Context.User);
 		    List<string> achievementsList = achievementManager.Achievements;
             if (achievementsList == null)
+			
+			return Json(GetAchievements(userProfile), JsonRequestBehavior.AllowGet);
+		}
+
+		public JsonResult GetUsers()
+		{
+			var users = Sitecore.Security.Accounts.UserManager.GetUsers();
+			List<UserAchievements> userAchievements  = new List<UserAchievements>();
+			foreach (var user in users)
+			{
+				userAchievements.Add(new UserAchievements{Name = user.Name, Achievements = GetAchievements(user.Profile as AchievementProfile)});
+			}
+			return Json(userAchievements, JsonRequestBehavior.AllowGet);
+		}
+
+		private List<Achievement> GetAchievements(AchievementProfile userProfile)
+		{if (userProfile == null)
 			{
 				return null;
 			}
@@ -33,9 +58,9 @@ namespace Hackathon.sitecore.shell.client.YourApps.hackathon2015_admin_b
 			{
 				Database db = Sitecore.Data.Database.GetDatabase("master");
 				var item = db.GetItem(achievementGuid);
-				achievements.Add(new Achievement { Name = item.GetField("Title"), Image = ThemeManager.GetImage(item.GetField("Icon"), 32, 32) });
+				achievements.Add(new Achievement { Name = item.GetField("Title"), Image = ThemeManager.GetImage(item.GetField("Icon"), 32, 32, "", "", item.GetField("Description"),"", true), Description = item.GetField("Description") });
 			}
-			return Json(achievements, JsonRequestBehavior.AllowGet);
+			return achievements;
 		}
 	}
 
@@ -43,6 +68,13 @@ namespace Hackathon.sitecore.shell.client.YourApps.hackathon2015_admin_b
 	{
 		public string Name { get; set; }
 		public string Image { get; set; }
+		public string Description { get; set; }
 	
+	}
+
+	public class UserAchievements
+	{
+		public string Name { get; set; }
+		public List<Achievement> Achievements { get; set; } 
 	}
 }
