1c9e5388df4828254cea32fd238634f2692c67ee
AllTheAction <alex.jackson@velir.com>
2015-01-24 13:11:06 -0500

    Whitespace update

--- Hackathon/sitecore/shell/client/YourApps/hackathon2015-admin-b/AchievementPageCode.js
+++ Hackathon/sitecore/shell/client/YourApps/hackathon2015-admin-b/AchievementPageCode.js
@@ -1,20 +1,20 @@
 ﻿define(["sitecore"], function (Sitecore) {
-  var AchievementPageCode = Sitecore.Definitions.App.extend({
-  	updateListing:function() {
+	var AchievementPageCode = Sitecore.Definitions.App.extend({
+		updateListing: function () {
 
-  		var app = this;
-  		var method;
-  		if (app.ToggleButton1.attributes.isOpen) {
-  			method = "getachievements";
-  			app.ToggleButton1.attributes.Text = "All achievements";
-  		} else {
-  			method = "getmyachievements";
-  			app.ToggleButton1.attributes.Text = "My achievements";
-  		}
+			var app = this;
+			var method;
+			if (app.ToggleButton1.attributes.isOpen) {
+				method = "getachievements";
+				app.ToggleButton1.set("text", "All achievements");
+			} else {
+				method = "getmyachievements";
+				app.ToggleButton1.set("text", "My achievements");
+			}
 
-  		app.AchievementDashboard1.refresh(method);
-  	}
-  });
+			app.AchievementDashboard1.refresh(method);
+		}
+	});
 
-  return AchievementPageCode;
+	return AchievementPageCode;
 });
