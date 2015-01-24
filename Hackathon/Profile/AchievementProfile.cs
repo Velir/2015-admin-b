c6bb41a085117df073c2e5e23ec74649f1db1c7d
ccaplette <corey@velir.com>
2015-01-24 12:11:45 -0500

    1) Updated Achievement Tracker Package
    2) Set Profile back to Sitecore default in web.config
    3) Updated AchievementProfile helper class with first three rules

--- Hackathon/Profile/AchievementProfile.cs
+++ Hackathon/Profile/AchievementProfile.cs
@@ -2,46 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
 
 namespace Hackathon.Profile
 {
-    public class AchievementProfile : Sitecore.Security.UserProfile
+    public class AchievementProfile
     {
-        private static string CreateFirstTestGuid = "{F9810B88-B838-4AE9-BA5B-9E3CB151F05B}";
-        private static string delimiter = "|";
-
-        public string Achievement
+        public List<string> Achievements
         {
             get
             {
-                return GetCustomProperty("achievement");
-            }
-            set
-            {
-                SetCustomProperty("achievement", value);
-                Save();
+                List<string> returnList = new List<string>();
+                string propertyValues = _achievements;
+                if (String.IsNullOrEmpty(propertyValues))
+                {
+                    return returnList;
+                }
+
+                return JsonConvert.DeserializeObject<List<String>>(propertyValues);
             }
         }
 
-        public string CreateFirstTestAchievement
+        private string _achievements
         {
+            get
+            {
+                return Sitecore.Context.User.Profile.GetCustomProperty("achievements");
+            }
             set
             {
-                string propertyValues = Achievement;
-                if (String.IsNullOrEmpty(propertyValues))
-                {
-                    return;
-                }
-
-                List<string> propertyValueList = propertyValues.Split('|').ToList();
-                if (!propertyValues.Contains(CreateFirstTestGuid))
-                {
-                    propertyValueList.Add(CreateFirstTestGuid);
-                }
-                propertyValues = String.Join(delimiter, propertyValues);
-                Achievement = propertyValues;
-                Save();
+                Sitecore.Context.User.Profile.SetCustomProperty("achievements", value);
+                Sitecore.Context.User.Profile.Save();
             }
+        }
+
+        public void ClearAchievements()
+        {
+            _achievements = string.Empty;
+        }
+        public void ActivateFirstABTestAchievement()
+        {
+            const string createFirstAbTestGuid = "{F9810B88-B838-4AE9-BA5B-9E3CB151F05B}";
+            _achievements = SetCustomAchievement(createFirstAbTestGuid);
+        }
+
+        public void ActivateFirstPersonalizationRuleAchievement()
+        {
+            const string createFirstPersonalizationRuleGuid = "{6C3DC925-CD85-4A9D-A6B6-AE4259537A62}";
+            _achievements = SetCustomAchievement(createFirstPersonalizationRuleGuid);
+        }
+
+        public void ActivateProfileFirstContentItemAchievement()
+        {
+            const string profileFirstContentItemGuid = "{C247CC31-0079-45A6-9BF0-6FDA6BE1F4C2}";
+            _achievements = SetCustomAchievement(profileFirstContentItemGuid);
+        }
+
+        private string SetCustomAchievement(string achievementGuid)
+        {
+            string propertyValues = _achievements;
+
+            List<String> achievements = JsonConvert.DeserializeObject<List<String>>(propertyValues);
+
+            if (achievements == null)
+            {
+                achievements = new List<string>();
+            }
+
+            if (!achievements.Contains(achievementGuid))
+            {
+                achievements.Add(achievementGuid);
+            }
+
+            return JsonConvert.SerializeObject(achievements);
         }
 
 
